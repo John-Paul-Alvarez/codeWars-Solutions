fa660103b8e8de2d8603ea89a0b8a35bc1fef7b0
AcePyurio <johnpaulalvarez111@gmail.com>
2024-04-28 19:18:40 -0400

    add 7kyu SortNumber.cpp

---
+++ 7kyu/SortNumber.cpp
@@ -0,0 +1,30 @@
+/*
+Description:
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+sortNumbers({1, 2, 10, 50, 5}) // sholud return {1, 2, 5, 10, 50}
+sortNumbers({}) // should return {}
+
+*/
+
+#include <vector>
+
+std::vector<int> solution(std::vector<int> nums)
+{
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] > nums[j])
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+    return nums;
+}
