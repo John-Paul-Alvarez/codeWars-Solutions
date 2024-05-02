381eec98744e7e674c752d3dc026c2e6a1844bef
AcePyurio <johnpaulalvarez111@gmail.com>
2024-05-02 16:25:50 -0400

    8 kyu

---
+++ 8kyu/What_is_between.cpp
@@ -0,0 +1,24 @@
+/*
+Description:
+
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+
+*/
+#include <vector>
+
+std::vector<int> between(int start, int end)
+{
+    std::vector<int> retVal;
+    for (int i = start; i < (end + 1); i++)
+    {
+        retVal.push_back(i);
+    }
+    return retVal;
+}
